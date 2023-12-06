bce4d1dae1147b107c1faa363c511a1f5e895e1f
EL kharmoudi Chadia <134792897+chadia08@users.noreply.github.com>
2023-12-06 22:49:53 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,3 +2,9 @@
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
+
+ # binary_tree_balance - A function that measures the balance factor of a
+ # binary tree.
+ # @tree: Pointer to root node to calculate balance factor.
+ # Return: The balance factor of the tree.
+ #
